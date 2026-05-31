c7ba492282bb0072a3f4d3c5df3d035690938eda
Chemidios <jopugu@gmail.com>
2026-05-31 20:59:45 +0200

    Commit Final

--- app/src/main/java/com/example/easytournament/ui/theme/Color.kt
+++ app/src/main/java/com/example/easytournament/ui/theme/Color.kt
@@ -2,19 +2,19 @@ package com.example.easytournament.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Paleta MediaMarkt
-val MMRed = Color(0xFFDF0000) // El rojo corporativo
+/* Paleta Estándar */
+val MMRed = Color(0xFFDF0000)
 val MMWhite = Color(0xFFFFFFFF)
 val MMBlack = Color(0xFF000000)
 val MMBordeaux = Color(0xFF800000)
 
-// Modo Claro
-val LightBackground = Color(0xFFF5F5F5) // Un gris muy muy claro para que el blanco destaque
+/* Modo Claro */
+val LightBackground = Color(0xFFF5F5F5)
 val LightSurface = Color(0xFFFFFFFF)
 val LightOnSurface = Color(0xFF1A1A1A)
 
-// Modo Oscuro
-val DarkBackground = Color(0xFF121212) // Negro material
-val DarkSurface = Color(0xFF1E1E1E)    // Gris oscuro para tarjetas
-val DarkOnSurface = Color(0xFFEEEEEE)   // Texto casi blanco
+/* Modo Oscuro */
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnSurface = Color(0xFFEEEEEE)
 
